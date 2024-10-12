54a576df3b2f99537ebc4683929fd064e1e567dd
Miler_2 <vegavegewtu@gmail.com>
2024-10-12 18:12:01 +0200

    I have added the first components

---
+++ src/app/components/landpage/landpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './landpage.component.html',
+  styleUrl: './landpage.component.css'
+})
+export class LandpageComponent {
+
+}
