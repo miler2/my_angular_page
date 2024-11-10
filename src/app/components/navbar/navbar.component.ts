ae79141e4757f53f82d28197848443ab83fc0178
Miler_2 <vegavegewtu@gmail.com>
2024-11-10 14:19:57 +0100

    I have added a button to the navbar (not functional yet)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
-import { AppComponent } from '../../app.component';
+import { Component, AfterContentInit } from '@angular/core';
+import { NavbarRoutingServiceService } from '../../services/navbar-routing-service.service';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterContentInit{
+  in_cv!: boolean;
 
+  constructor(
+    private router: Router,
+    private navbarRoutingService: NavbarRoutingServiceService
+  )
+  {}
+
+  ngAfterContentInit(){
+    // Listen to route changes using Router events
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.navbarRoutingService.is_user_in_mi_cv(); // This initializes the variable of the serice checking if the user is in the "/mi_cv" url or not.
+        
+        // This is a test for checking if we are in the right url. It'll show on the angular console.
+        // console.log('Current URL: ' + this.router.url);
+      });
+
+    // I listen to the variable initialized just before so that every change to that variable also changes the local one.
+    this.navbarRoutingService.data$.subscribe({
+      next: (data) => {
+        this.in_cv = data;
+      }
+    })
+  }
+
+  update_in_cv(){
+    this.navbarRoutingService.is_user_in_mi_cv();
+  }
+
+
+  // PAGE THEMES
   changeTheme(){
     const themeElement = document.body;
     const theme = themeElement.getAttribute('data-bs-theme');
@@ -19,8 +54,7 @@ export class NavbarComponent {
     } else if (theme === "light"){
       themeElement.setAttribute('data-bs-theme', 'dark');
     } else {
-      alert('There was an error changing the theme...')
-      // alert(`Current theme is: ${theme}`);
+      alert('There was an error changing the theme...');
     }
   }
 }
